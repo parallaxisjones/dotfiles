1d6243c94e06142ec6ece4bb6c0f71fe06b93e08
Parker Jones <parker.emailaddress+github@gmail.com>
2025-08-27 16:07:59 -0400

    Statix: consolidate repeated keys; ignore generated hardware-configuration.nix @ai-code (#6)

--- hosts/nixos/hardware-configuration.nix
+++ hosts/nixos/hardware-configuration.nix
@@ -7,7 +7,6 @@
   imports =
     [ (modulesPath + "/installer/scan/not-detected.nix")
     ];
-
   boot.initrd.availableKernelModules = [ "xhci_pci" "ahci" "usbhid" "usb_storage" "sd_mod" ];
   boot.initrd.kernelModules = [ ];
   boot.kernelModules = [ "kvm-amd" ];
