68f126c9586a09f89b98f93f9c495d2e35581db7
Parker Jones <parker.jones@rithum.com>
2025-08-27 17:06:24 -0400

    feat(nixos): enable OpenSSH server @ai-code
    - Set services.openssh.enable = true\n- Firewall already allows 22/tcp in networking.firewall.allowedTCPPorts\n- Verified via flake eval: nix eval .#nixosConfigurations.x86_64-linux.config.services.openssh.enable -> true

--- hosts/nixos/configuration.nix
+++ hosts/nixos/configuration.nix
@@ -149,7 +149,7 @@
   # List services that you want to enable:
 
   # Enable the OpenSSH daemon.
-  # services.openssh.enable = true;
+  services.openssh.enable = true;
 
   # Open ports in the firewall moved into networking set above
   # networking.firewall.allowedUDPPorts = [ ... ];
