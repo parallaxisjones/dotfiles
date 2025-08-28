601bd8aebc21c4fa2121422d3f4df26bf8d79ba3
Parker Jones <parker.jones@rithum.com>
2025-08-28 12:01:40 -0400

    NOJIRA: split NixOS packages into base shared and desktop-specific; keep servers lean @ai-code

---
+++ modules/nixos/packages-desktop.nix
@@ -0,0 +1,59 @@
+{ pkgs }:
+
+with pkgs; [
+  # Desktop packages specific to NixOS desktop role
+  _1password-gui
+  yubikey-agent
+  keepassxc
+  barrier
+  appimage-run
+  gnumake
+  cmake
+  home-manager
+  steam
+  gimp
+  vlc
+  wineWowPackages.stable
+  fontconfig
+  font-manager
+  brlaser
+  bc
+  galculator
+  pavucontrol
+  cider
+  discord
+  hexchat
+  fractal
+  cypress
+  chromedriver
+  direnv
+  rofi
+  rofi-calc
+  qmk
+  postgresql
+  libusb1
+  libtool
+  flameshot
+  simplescreenrecorder
+  emote
+  feh
+  screenkey
+  xclip
+  xorg.xwininfo
+  xorg.xrandr
+  inotify-tools
+  i3lock-fancy-rapid
+  libnotify
+  ledger-live-desktop
+  playerctl
+  pcmanfm
+  xdg-utils
+  yad
+  xdotool
+  google-chrome
+  zathura
+  spotify
+  immersed
+]
+
+
