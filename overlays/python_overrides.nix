6b5a7891a94dae7e565750ba5e5e6626c3762a2e
Parker Jones <parker.jones@rithum.com>
2025-08-28 00:47:25 -0400

    fix: formatted overlay

--- overlays/python_overrides.nix
+++ overlays/python_overrides.nix
@@ -1,14 +1,14 @@
 final: prev:
-  if prev.stdenv.isDarwin then
-    {
-      # Disable failing tests for aiohttp on Darwin to unblock builds.
-      python3Packages = prev.python3Packages // {
-        aiohttp = prev.python3Packages.aiohttp.overrideAttrs (_: {
-          doCheck = false;
-        });
-      };
-    }
-  else
-    {}
+if prev.stdenv.isDarwin then
+  {
+    # Disable failing tests for aiohttp on Darwin to unblock builds.
+    python3Packages = prev.python3Packages // {
+      aiohttp = prev.python3Packages.aiohttp.overrideAttrs (_: {
+        doCheck = false;
+      });
+    };
+  }
+else
+  { }
 
 
