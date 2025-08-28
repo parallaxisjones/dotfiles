b0f5df95681b304cdc1c36998f26f9db28d41c59
Parker Jones <parker.jones@rithum.com>
2025-08-28 01:04:33 -0400

    NOJIRA: disable fsspec tests on Linux; compose platform overlays @ai-code

--- overlays/python_overrides.nix
+++ overlays/python_overrides.nix
@@ -1,14 +1,27 @@
 _: prev:
-if prev.stdenv.isDarwin then
-  {
-    # Disable failing tests for aiohttp on Darwin to unblock builds.
-    python3Packages = prev.python3Packages // {
-      aiohttp = prev.python3Packages.aiohttp.overrideAttrs (_: {
-        doCheck = false;
-      });
-    };
-  }
-else
-  { }
+  # Compose per-platform overrides
+  (if prev.stdenv.isDarwin then
+    {
+      # Disable failing tests for aiohttp on Darwin to unblock builds.
+      python3Packages = prev.python3Packages // {
+        aiohttp = prev.python3Packages.aiohttp.overrideAttrs (_: {
+          doCheck = false;
+        });
+      };
+    }
+  else
+    { })
+  //
+  (if prev.stdenv.isLinux then
+    {
+      # Disable failing tests for fsspec on Linux (transitive for aider-chat)
+      python3Packages = prev.python3Packages // {
+        fsspec = prev.python3Packages.fsspec.overrideAttrs (_: {
+          doCheck = false;
+        });
+      };
+    }
+  else
+    { })
 
 
