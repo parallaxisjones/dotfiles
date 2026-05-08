51ede6173ce911ab9e8d6bcbef948bcc1240187a
Parker Jones <parker.jones@rithum.com>
2026-05-08 16:57:28 -0400

    rewrite lsp config

--- modules/shared/config/nvim/lua/plugins/configs/lspconfig.lua
+++ modules/shared/config/nvim/lua/plugins/configs/lspconfig.lua
@@ -1,26 +1,10 @@
 dofile(vim.g.base46_cache .. "lsp")
 require "nvchad.lsp"
 
-local M = {}
 local utils = require "core.utils"
 
--- export on_attach & capabilities for custom lspconfigs
-
-M.on_attach = function(client, bufnr)
-  utils.load_mappings("lspconfig", { buffer = bufnr })
-
-  if client.server_capabilities.signatureHelpProvider then
-    require("nvchad.signature").setup(client)
-  end
-
-  if not utils.load_config().ui.lsp_semantic_tokens and client.supports_method "textDocument/semanticTokens" then
-    client.server_capabilities.semanticTokensProvider = nil
-  end
-end
-
-M.capabilities = vim.lsp.protocol.make_client_capabilities()
-
-M.capabilities.textDocument.completion.completionItem = {
+local capabilities = vim.lsp.protocol.make_client_capabilities()
+capabilities.textDocument.completion.completionItem = {
   documentationFormat = { "markdown", "plaintext" },
   snippetSupport = true,
   preselectSupport = true,
@@ -38,10 +22,31 @@ M.capabilities.textDocument.completion.completionItem = {
   },
 }
 
-require("lspconfig").lua_ls.setup {
-  on_attach = M.on_attach,
-  capabilities = M.capabilities,
+vim.lsp.config("*", { capabilities = capabilities })
 
+vim.api.nvim_create_autocmd("LspAttach", {
+  group = vim.api.nvim_create_augroup("user_lsp_attach", { clear = true }),
+  callback = function(args)
+    local bufnr = args.buf
+    local client = vim.lsp.get_client_by_id(args.data.client_id)
+    if not client then
+      return
+    end
+
+    utils.load_mappings("lspconfig", { buffer = bufnr })
+
+    if client.server_capabilities.signatureHelpProvider then
+      require("nvchad.signature").setup(client)
+    end
+
+    if not utils.load_config().ui.lsp_semantic_tokens
+      and client:supports_method "textDocument/semanticTokens" then
+      client.server_capabilities.semanticTokensProvider = nil
+    end
+  end,
+})
+
+vim.lsp.config("lua_ls", {
   settings = {
     Lua = {
       diagnostics = {
@@ -59,6 +64,6 @@ require("lspconfig").lua_ls.setup {
       },
     },
   },
-}
+})
 
-return M
+vim.lsp.enable "lua_ls"
