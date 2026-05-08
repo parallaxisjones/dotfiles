744a053839ae8ca7320ab3e5b3b77b6805e084cc
Parker Jones <parker.jones@rithum.com>
2026-05-08 16:49:07 -0400

    fix neovim treesitter

--- modules/shared/config/nvim/lua/plugins/init.lua
+++ modules/shared/config/nvim/lua/plugins/init.lua
@@ -73,7 +73,6 @@ local default_plugins = {
 
   {
     "nvim-treesitter/nvim-treesitter",
-    tag = "v0.9.2",
     init = function()
       require("core.utils").lazy_load "nvim-treesitter"
     end,
