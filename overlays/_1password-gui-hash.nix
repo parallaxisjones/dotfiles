f9df7a2615967a7660df4ba451234c5c11018139
Parker Jones <parker.jones@rithum.com>
2026-09-24 17:58:03 -0400

    fix: remove stale 1Password hash and stop failed Darwin builds

--- overlays/_1password-gui-hash.nix
+++
@@ -1,9 +0,0 @@
-_: prev:
-if prev.stdenv.isDarwin then {
-  _1password-gui = prev._1password-gui.overrideAttrs (old: {
-    src = prev.fetchurl {
-      inherit (old.src) url;
-      hash = "sha256-bZD8LCLTGXRpNF/FqoSHvI69pquAcQGa1mdagWypgDU=";
-    };
-  });
-} else { }
